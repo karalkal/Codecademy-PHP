9911e3a84188af5d30ffc89fd59744527b3f40b7
karalkal <karalkal@yahoo.co.uk>
2024-05-08 23:11:09 +0100

    examples for ordered arrays basics

---
+++ 07_ordered_arrays.php
@@ -0,0 +1,48 @@
+<?php
+
+$number_array = [0, 1, 2];
+echo $number_array;     // Prints: Array
+echo "\n";
+print_r($number_array);
+
+echo "\n\n";
+
+$message = ["Oh hey", " You're doing great", " Keep up the good work!\n"];
+echo implode("!", $message);
+
+echo "\n\n";
+// add elements to the end with square brackets without index
+// replace - with index
+$change_me = [3, 6, 9];
+$change_me[] = "OPPA";  //append
+$change_me[] = 88;
+$change_me[1] = "Replaced!";   //replace
+print_r($change_me);
+
+echo "\n\n";
+// array_pop() - same as other languages
+// array_push() function takes an array as its first argument. The arguments that follow are elements to be added to the end of the array.
+$stack = [1, 2, 3];
+array_push($stack, "AA", "BB");
+print_r($stack);
+echo "\n";
+$first_popped = array_pop($stack);
+echo "\$first_popped: ", $first_popped, "\n";
+array_pop($stack);
+array_pop($stack);
+array_pop($stack);
+print_r($stack);
+
+echo "\n\n";
+// array_shift() - removes the first element of an array and returns that value
+// array_unshift() takes an array as its first argument. The arguments that follow are elements to be added to the beginning of the array. 
+$record_holders = [];
+array_unshift($record_holders, "Tim Montgomery", "Maurice Greene", "Donovan Bailey", "Leroy Burrell", "Carl Lewis");
+echo implode(", ", $record_holders) . "\n\n";
+$shifted_guy = array_shift($record_holders);
+echo "\$shifted_guy: ", $shifted_guy, "\n";
+echo implode(", ", $record_holders) . "\n\n";
+
+echo "\n\n";
+
+
